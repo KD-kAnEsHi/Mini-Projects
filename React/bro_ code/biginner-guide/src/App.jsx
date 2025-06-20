6cfc79059a80cd6568f3d4e4ce34346329f57232
Lonely Man I AM <112342120+KD-kAnEsHi@users.noreply.github.com>
2025-06-20 12:06:35 -0500

    bro-code intro to react

---
+++ React/bro_ code/biginner-guide/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header.jsx";
+import Food from "./Food.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <Food />
+      <Food />
+      <Food />
+      <Food />
+      <Food />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ React/bro_ code/biginner-guide/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} your Website name</p>
+        </footer>
+    );
+}
+export default Footer
